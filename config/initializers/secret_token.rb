37674a2195f3ff608e134bc9e36a6bbfacaaf33c
Dan Bikle <dan@bot4.us>
2012-04-09 09:31:51 +0100

    j4_z3_2012_0409_c1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+J4::Application.config.secret_token = '44b1c7d92c9bd60cc90862646f720464236ad0276634978d98b86575eb7c7abc3bdb2ba0dd00e2aeeee5724b74982282befb588948d4fd6e2b65f51f812972c8'
