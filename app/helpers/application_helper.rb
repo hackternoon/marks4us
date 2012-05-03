8464b0f6b78cdb3491ab32c45bdd313d01f58612
Dan Bikle <dan@bot4.us>
2012-05-03 22:34:17 +0100

    layouts/application.html.haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+
+  # If I own an object, I often want to edit it.
+  def i_own?(object)
+    if(current_user.present? and object.user.present? and object.user.id.present? and (current_user.id == object.user.id))
+      true
+    else
+      false
+    end
+  end # def i_own?(object)
+
+  # If I dont own an object, I sometimes want to know that.
+  def i_dont_own?(object)
+    if(current_user.present? and object.user.present? and object.user.id.present? and (current_user.id == object.user.id))
+      false
+    else
+      true
+    end
+  end # def i_dont_own?(object)
+
+  # In production I want users talking to https://m41.herokuapp.com
+  def sslhost
+    if ENV['RAILS_ENV'] == 'production'
+      "https://m41.herokuapp.com"
+    else
+      ""
+    end
+  end # def sslhost
 end
+
