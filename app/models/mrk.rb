3531fd3be6778fc282ad976c6fd92805bd6d8416
Dan Bikle <dan@bot4.us>
2012-05-05 19:27:34 +0100

    MrksController has no json now

--- app/models/mrk.rb
+++ app/models/mrk.rb
@@ -1,3 +1,6 @@
 class Mrk < ActiveRecord::Base
-  attr_accessible :note, :tag1, :tag2, :tag3, :user_id, :usr_id, :uurl
+  # Associations before white-list:
+  belongs_to :usr
+  belongs_to :user
+  attr_accessible :note, :tag1, :tag2, :tag3, :uurl
 end
--- app/models/usr.rb
+++ app/models/usr.rb
@@ -1,4 +1,6 @@
 class Usr < ActiveRecord::Base
+  # Associations before white-list:
+  has_many :mrks
   attr_accessible :name, :password, :password_confirmation
   validates :name, presence: true, uniqueness: true
   has_secure_password
