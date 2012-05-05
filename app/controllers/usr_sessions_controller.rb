0c8dd8d707c11fafdd48db7feb890b0031304b45
Dan Bikle <dan@bot4.us>
2012-05-05 05:42:25 +0100

    Enhance logout

--- app/controllers/usr_sessions_controller.rb
+++ app/controllers/usr_sessions_controller.rb
@@ -5,11 +5,28 @@ class UsrSessionsController < Devise::SessionsController
 
   def create
 debugger
-    super
+    # See if person wants to authenticate a Usr-object:
+    @usr = Usr.find_by_name(params[:user][:email]).try(:authenticate, params[:user][:password])
+    if @usr.present?
+      session[:usr] = @usr.name
+      return
+    else
+      # See if person wants to authenticate a User-object.
+      # I'll ask Devise to do the work:
+      super
+      if current_user.present?
+        session[:usr] = current_user.email
+      else
+        session[:usr] = nil
+      end
+    end
   end
 
   def destroy
 debugger
-    super
+    # If the person has a current_user object, I'll let Devise destroy the session:
+    super if current_user.present?
+    # If the person has a session[:usr], I set it to nil.
+    session[:usr] = nil
   end
 end
