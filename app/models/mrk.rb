7e79cf99d99795f312ee7f31de1ed37a2bea53a4
Dan Bikle <dan@bot4.us>
2012-05-06 06:52:25 +0100

    before_save :check_owner

--- app/models/mrk.rb
+++ app/models/mrk.rb
@@ -3,4 +3,14 @@ class Mrk < ActiveRecord::Base
   belongs_to :usr
   belongs_to :user
   attr_accessible :note, :tag1, :tag2, :tag3, :uurl
+  # Callbacks after Associations
+  before_save :check_owner
+
+  # I ensure that each @mrk belongs to at least usr 1 which should be Guest.
+  def check_owner
+    if(self.usr_id.blank? and self.user_id.blank?)
+      # usr 1 should be Guest which I create via db/seeds.rb
+      self.usr_id = 1
+    end
+  end # def check_owner
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Usr.create(name: 'Guest', :password => "abc123", :password_confirmation => "abc123")
+# Useful syntax for the console to change the pass:
+# usr = Usr.find 1
+# usr.password_confirmation = usr.password = 'xyz321'
+# usr.save
+# Sometimes the above call to usr.save will hang if someone else is connected to the db or the app.
