03f0d5c607f98a91bff732eb3bbc0ef15a2aa91c
Dan Bikle <dan@bot4.us>
2012-05-12 19:47:38 +0100

    js-true works!

--- spec/requests/mrks_spec.rb
+++ spec/requests/mrks_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Mrks" do
-  describe "GET /mrks" do
+  describe "GET /mrks", :js => true do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit mrks_path
