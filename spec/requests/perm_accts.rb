429ab60d866dc34fb1187a3fad5f97fd78e966b4
Dan Bikle <dan@bot4.us>
2012-05-13 19:24:03 +0100

    FactoryGirl many Dans

--- spec/requests/perm_accts.rb
+++ spec/requests/perm_accts.rb
@@ -6,6 +6,14 @@ describe "Permanent Accounts Page" do
       "/users/index".should == get_users_index_path
       get get_users_index_path
       response.status.should be(200)
+      @dan == FactoryGirl.create(:user, :email => 'dan@bot4.us')
+      visit get_users_index_path
+      page.should have_content "E-Mail"
+      page.should have_content "Number of Bookmarks"
+      page.should have_content 'dan@bot4.us'
+      (10..99).each{|n| FactoryGirl.create(:user,:email=>"dan#{n}@bot4.us")}
+      visit get_users_index_path
+      page.should have_content 'dan10@bot4.us'
     end
 
   end
