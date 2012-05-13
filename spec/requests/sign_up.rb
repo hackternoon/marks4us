3fc52567e11f75d7fa09ef7ddba75a5344e0d2f0
Dan Bikle <dan@bot4.us>
2012-05-13 08:54:04 +0100

    enhanced spec/requests/sign_up.rb

--- spec/requests/sign_up.rb
+++ spec/requests/sign_up.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "Sign Up Page" do
-  describe "GET /users/sign_up", :js => false do
-    it "Show me the Sign Up page" do
+  describe "GET /users/sign_up", :js => true do
+    it "Show me the User Sign Up form" do
       "/users/sign_up".should == new_user_registration_path
       get new_user_registration_path
       response.status.should be(200)
@@ -30,7 +30,35 @@ describe "Sign Up Page" do
       click_button 'Permanent Sign Up'
       page.should have_content "error"
       page.should have_content "Email has already been taken"
+    end
 
+    it "Show me the Usr Sign Up form" do
+      visit new_user_registration_path
+      fill_in('usr_name', :with => 'dan')
+      fill_in('usr_password', :with => 'abc123')
+      fill_in('usr_password_confirmation', :with => 'abc123')
+      click_button '2 Month Sign Up'
+      page.should have_content "Success!"
+      page.should have_content "Now, memorize your password"
+
+      click_link "marks4.us"
+      click_link "Sign Up"
+
+      fill_in('usr_name', :with => 'dan')
+      fill_in('usr_password', :with => 'abc123')
+      fill_in('usr_password_confirmation', :with => 'abc123')
+      click_button '2 Month Sign Up'
+      page.should have_content "error"
+      page.should have_content "Name has already been taken"
+
+      click_link "Sign In"
+      fill_in('user_email', :with => 'dan')
+      fill_in('user_password', :with => 'abc123')
+      click_button 'Sign in'
+
+debugger
+      page.should have_content "You are now signed in"
+      
     end
   end
 end
