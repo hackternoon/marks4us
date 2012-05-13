f9f1d6687ea02673c02d8d4f2527d1ebc694eb24
Dan Bikle <dan@bot4.us>
2012-05-13 09:14:00 +0100

    enhanced spec/requests/sign_up.rb

--- spec/requests/sign_up.rb
+++ spec/requests/sign_up.rb
@@ -19,6 +19,12 @@ describe "Sign Up Page" do
       fill_in('user_password_confirmation', :with => 'abc123')
       click_button 'Permanent Sign Up'
       page.should have_content "A message with a confirmation link has been sent to your email address"
+      @dan = User.find_by_email 'dan@bot4.us'
+      visit "/users/confirmation?confirmation_token=#{@dan.confirmation_token}"
+      page.should have_content "Your account was successfully confirmed"
+      click_link "marks4.us"
+      click_link "Sign Out"
+      page.should have_content "Signed out successfully"
       # Do it again and look for an error due to duplicate e-mail:
       click_link "marks4.us"
       click_link "Sign Up"
@@ -40,24 +46,22 @@ describe "Sign Up Page" do
       click_button '2 Month Sign Up'
       page.should have_content "Success!"
       page.should have_content "Now, memorize your password"
-
       click_link "marks4.us"
       click_link "Sign Up"
-
       fill_in('usr_name', :with => 'dan')
       fill_in('usr_password', :with => 'abc123')
       fill_in('usr_password_confirmation', :with => 'abc123')
       click_button '2 Month Sign Up'
       page.should have_content "error"
       page.should have_content "Name has already been taken"
-
       click_link "Sign In"
       fill_in('user_email', :with => 'dan')
       fill_in('user_password', :with => 'abc123')
       click_button 'Sign in'
-
-debugger
       page.should have_content "You are now signed in"
+      click_link "Sign Out"
+      page.should have_content "You are ..."
+      page.should have_content "Signed Out"
       
     end
   end
