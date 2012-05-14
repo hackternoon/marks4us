133fb54d52c4fb8be30f0157d1fe93d540bfd226
Dan Bikle <dan@bot4.us>
2012-05-13 17:03:32 -0700

    mrks spec

--- spec/requests/mrks_spec.rb
+++ spec/requests/mrks_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe "Mrks" do
   describe "GET /mrks", :js => true do
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit mrks_path
-      # response.status.should be(200)
       page.should have_content "Book Marks!"
+      @dan = FactoryGirl.create(:user, :email => 'dan@bot4.us')
+      m1 =   FactoryGirl.create(:mrk, :user_id => @dan.id, :usr_id => nil)
+      (10..99).each{|n| FactoryGirl.create(:mrk, :user_id => @dan.id, :usr_id => nil)
     end
   end
 end
